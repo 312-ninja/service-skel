ff69ed3f589d51438758b0b5be0baa87b2388ab9
Dave D <davey@localhost.localdomain>
2017-12-12 13:12:15 -0600

    remove <`>

--- service-skel.sh
+++ service-skel.sh
@@ -12,7 +12,7 @@ pidFile="$daemonName.pid"
 
 logDir="."
 # To use a dated log file.
-# logFile="$logDir/$daemonName-"`date +"%Y-%m-%d"`".log"
+# logFile="$logDir/$daemonName-"'date +"%Y-%m-%d"'".log"
 # To use a regular log file.
 logFile="$logDir/$daemonName.log"
 
@@ -30,7 +30,7 @@ doCommands() {
 # Below is the skeleton functionality of the daemon.
 ################################################################################
 
-myPid=`echo $$`
+myPid='echo $$'
 
 setupDaemon() {
   # Make sure that the directories work.
@@ -44,7 +44,7 @@ setupDaemon() {
     touch "$logFile"
   else
     # Check to see if we need to rotate the logs.
-    size=$((`ls -l "$logFile" | cut -d " " -f 8`/1024))
+    size=$(('ls -l "$logFile" | cut -d " " -f 8'/1024))
     if [[ $size -gt $logMaxSize ]]; then
       mv $logFile "$logFile.old"
       touch "$logFile"
@@ -55,13 +55,13 @@ setupDaemon() {
 startDaemon() {
   # Start the daemon.
   setupDaemon # Make sure the directories are there.
-  if [[ `checkDaemon` = 1 ]]; then
+  if [[ 'checkDaemon' = 1 ]]; then
     echo " * \033[31;5;148mError\033[39m: $daemonName is already running."
     exit 1
   fi
   echo " * Starting $daemonName with PID: $myPid."
   echo "$myPid" > "$pidFile"
-  log '*** '`date +"%Y-%m-%d"`": Starting up $daemonName."
+  log '*** ''date +"%Y-%m-%d"'": Starting up $daemonName."
 
   # Start the loop.
   loop
@@ -69,21 +69,21 @@ startDaemon() {
 
 stopDaemon() {
   # Stop the daemon.
-  if [[ `checkDaemon` -eq 0 ]]; then
+  if [[ 'checkDaemon' -eq 0 ]]; then
     echo " * \033[31;5;148mError\033[39m: $daemonName is not running."
     exit 1
   fi
   echo " * Stopping $daemonName"
-  log '*** '`date +"%Y-%m-%d"`": $daemonName stopped."
+  log '*** ''date +"%Y-%m-%d"'": $daemonName stopped."
 
-  if [[ ! -z `cat $pidFile` ]]; then
-    kill -9 `cat "$pidFile"` &> /dev/null
+  if [[ ! -z 'cat $pidFile' ]]; then
+    kill -9 'cat "$pidFile"' &> /dev/null
   fi
 }
 
 statusDaemon() {
   # Query and return whether the daemon is running.
-  if [[ `checkDaemon` -eq 1 ]]; then
+  if [[ 'checkDaemon' -eq 1 ]]; then
     echo " * $daemonName is running."
   else
     echo " * $daemonName isn't running."
@@ -93,7 +93,7 @@ statusDaemon() {
 
 restartDaemon() {
   # Restart the daemon.
-  if [[ `checkDaemon` = 0 ]]; then
+  if [[ 'checkDaemon' = 0 ]]; then
     # Can't restart it if it isn't running.
     echo "$daemonName isn't running."
     exit 1
@@ -108,9 +108,9 @@ checkDaemon() {
   # so that we can use it other functions.
   if [ -z "$oldPid" ]; then
     return 0
-  elif [[ `ps aux | grep "$oldPid" | grep -v grep` > /dev/null ]]; then
+  elif [[ 'ps aux | grep "$oldPid" | grep -v grep' > /dev/null ]]; then
     if [ -f "$pidFile" ]; then
-      if [[ `cat "$pidFile"` = "$oldPid" ]]; then
+      if [[ 'cat "$pidFile"' = "$oldPid" ]]; then
         # Daemon is running.
         # echo 1
         return 1
@@ -119,9 +119,9 @@ checkDaemon() {
         return 0
       fi
     fi
-  elif [[ `ps aux | grep "$daemonName" | grep -v grep | grep -v "$myPid" | grep -v "0:00.00"` > /dev/null ]]; then
+  elif [[ 'ps aux | grep "$daemonName" | grep -v grep | grep -v "$myPid" | grep -v "0:00.00"' > /dev/null ]]; then
     # Daemon is running but without the correct PID. Restart it.
-    log '*** '`date +"%Y-%m-%d"`": $daemonName running with invalid PID; restarting."
+    log '*** ''date +"%Y-%m-%d"'": $daemonName running with invalid PID; restarting."
     restartDaemon
     return 1
   else
@@ -133,10 +133,10 @@ checkDaemon() {
 
 loop() {
   # This is the loop.
-  now=`date +%s`
+  now='date +%s'
 
   if [ -z $last ]; then
-    last=`date +%s`
+    last='date +%s'
   fi
 
   # Do everything you need the daemon to do.
@@ -145,7 +145,7 @@ loop() {
   # Check to see how long we actually need to sleep for. If we want this to run
   # once a minute and it's taken more than a minute, then we should just run it
   # anyway.
-  last=`date +%s`
+  last='date +%s'
 
   # Set the sleep interval
   if [[ ! $((now-last+runInterval+1)) -lt $((runInterval)) ]]; then
@@ -167,7 +167,7 @@ log() {
 ################################################################################
 
 if [ -f "$pidFile" ]; then
-  oldPid=`cat "$pidFile"`
+  oldPid='cat "$pidFile"'
 fi
 checkDaemon
 case "$1" in
